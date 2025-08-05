955021d828b879c79b574f9805ab5206dd2d4328
coco-kit <bofg.fu@outlook.com>
2025-08-05 15:46:23 -0230

    fix build error

--- app/judge/components/judge-layout-client.tsx
+++ app/judge/components/judge-layout-client.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { JudgeSidebar } from './judge-sidebar'
 import { JudgeHeader } from './judge-header'
 import { TooltipProvider } from '@/components/ui/tooltip'
