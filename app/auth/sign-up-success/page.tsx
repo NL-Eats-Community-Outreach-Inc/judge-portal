6a4767d27fff72b4b03bf921d04ef1a97dbb8234
Abey12525 <abeyhurtis@gmail.com>
2025-09-02 14:37:54 -0230

    feat: added sign in button on sign up success
    feat: added back button to navigate home on sign up success

--- app/auth/sign-up-success/page.tsx
+++ app/auth/sign-up-success/page.tsx
@@ -5,6 +5,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -23,8 +26,28 @@ export default function Page() {
                 You&apos;ve successfully signed up. Please check your email to
                 confirm your account before signing in.
               </p>
+
+              <Button asChild size="lg" variant="outline" className="border-gray-300 dark:border-gray-700 w-full mt-5">
+                <Link href="/auth/login">
+                  Sign In
+                </Link>
+              </Button>
             </CardContent>
           </Card>
+          <div className="flex justify-center items-center flex-col mt-1">
+            <Button
+              asChild
+              size="sm"
+              className="bg-transparent hover:bg-transparent  
+                  hover:scale-150 transition-transform duration-200 
+                  text-gray-900 dark:text-gray-100 rounded-full w-10 h-10 p-0
+                  flex items-center justify-center"
+            >
+              <Link href="/" className="flex items-center gap-2">
+                <ArrowLeft className="w-4 h-4" />
+              </Link>
+            </Button>
+          </div>
         </div>
       </div>
     </div>
