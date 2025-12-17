9584dd8ad07a719e8780044e62afc7a85dce4379
SilverByte <johnryan1262@gmail.com>
2025-12-17 11:52:05 -0330

    Fix up Learnworlds API

--- lib/learnworlds.ts
+++ lib/learnworlds.ts
@@ -1,20 +1,17 @@
 import type { Course } from '@/app/participant/knowledge-hub/components/course-card';
 
-/**
- * TODO:
- * Get Learnworlds endpoint and client ID so I can actually
- * implement the courses and API stuff.
- */
-
-const BASE = 'https://api.learnworlds.com';
-const API_KEY = process.env.NEXT_PUBLIC_LEARNWORLDS_API_KEY;
+const BASE = 'https://stoplight.io/mocks/learnworlds/api/2951998/v2';
+const API_KEY = process.env.LEARNWORLDS_API_KEY;
+const LW_CLIENT_ID = process.env.LEARNWORLDS_CLIENT_ID;
 
 export const api = {
-  async getCourses(page = 1, perPage = 9) {
-    const res = await fetch(`${BASE}/courses?page=${page}&per_page=${perPage}`, {
+  async getCourses(page = 1) {
+    const res = await fetch(`${BASE}/courses?page=${page}`, {
+      method: 'GET',
       headers: {
+        'Lw-Client': `${LW_CLIENT_ID}`,
         Authorization: `Bearer ${API_KEY}`,
-        'Content-Type': 'application/json',
+        Accept: 'application/json',
       },
       cache: 'no-store',
     });
@@ -35,9 +32,9 @@ export const api = {
 
     return {
       courses,
-      totalPages: data.total_pages,
+      totalPages: data.totalPages,
       currentPage: data.page,
-      total: data.total,
+      totalItems: data.totalItems,
     };
   },
 };
